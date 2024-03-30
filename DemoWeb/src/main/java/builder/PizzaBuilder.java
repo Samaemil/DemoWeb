c11d1d3f0a71644c4808fa990948b0fa463cf961
DELL <DELL@192.168.1.23>
2024-03-30 13:06:45 +0200

    initial commit

---
+++ DemoWeb/src/main/java/builder/PizzaBuilder.java
@@ -0,0 +1,8 @@
+package builder;
+
+public interface PizzaBuilder {
+	PizzaBuilder setSize(String size);
+    PizzaBuilder setCrust(String crust);
+    PizzaBuilder addTopping(String topping);
+    Pizza build();
+}
